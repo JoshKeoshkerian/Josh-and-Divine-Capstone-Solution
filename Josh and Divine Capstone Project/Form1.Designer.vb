7282e6290befb395a1c2bc0ef8203f84c67c74a7
JoshKeoshkerian <JKeoshke3745@slps.org>
2023-04-28 09:22:16 -0500

    Add project files.

---
+++ Josh and Divine Capstone Project/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Josh_and_Divine_Capstone_Project.My.Resources.Resources._69998644_EE6F_43B7_A43A_49E98CDE9CC7_removebg_preview
+        Me.PictureBox1.Location = New System.Drawing.Point(31, 45)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(189, 388)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'pic1
+        '
+        Me.pic1.Image = Global.Josh_and_Divine_Capstone_Project.My.Resources.Resources.O1
+        Me.pic1.Location = New System.Drawing.Point(254, 34)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(816, 699)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 0
+        Me.pic1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1082, 745)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pic1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents Timer As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Josh and Divine Capstone Project/Form1.vb
@@ -0,0 +1,116 @@
+﻿Public Class Form1
+
+    Dim Value As Integer
+    Dim Number As Integer
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+
+        Number += 1
+        If Number = Value Then
+            Timer.Enabled = False
+        End If
+
+        If Number Mod 24 = 0 Then
+            pic1.Image = My.Resources.O1
+            SpinWheel()
+        ElseIf Number Mod 24 = 1 Then
+            pic1.Image = My.Resources.O2
+            SpinWheel()
+        ElseIf Number Mod 24 = 2 Then
+            pic1.Image = My.Resources.O3
+            SpinWheel()
+        ElseIf Number Mod 24 = 3 Then
+            pic1.Image = My.Resources.O4
+            SpinWheel()
+        ElseIf Number Mod 24 = 4 Then
+            pic1.Image = My.Resources.O5
+            SpinWheel()
+        ElseIf Number Mod 24 = 5 Then
+            pic1.Image = My.Resources.O6
+            SpinWheel()
+        ElseIf Number Mod 24 = 6 Then
+            pic1.Image = My.Resources.O7
+            SpinWheel()
+        ElseIf Number Mod 24 = 7 Then
+            pic1.Image = My.Resources.O8
+            SpinWheel()
+        ElseIf Number Mod 24 = 8 Then
+            pic1.Image = My.Resources.O9
+            SpinWheel()
+        ElseIf Number Mod 24 = 9 Then
+            pic1.Image = My.Resources.O10
+            SpinWheel()
+        ElseIf Number Mod 24 = 10 Then
+            pic1.Image = My.Resources.O11
+            SpinWheel()
+        ElseIf Number Mod 24 = 11 Then
+            pic1.Image = My.Resources.O12
+            SpinWheel()
+        ElseIf Number Mod 24 = 12 Then
+            pic1.Image = My.Resources.O13
+            SpinWheel()
+        ElseIf Number Mod 24 = 13 Then
+            pic1.Image = My.Resources.O14
+            SpinWheel()
+        ElseIf Number Mod 24 = 14 Then
+            pic1.Image = My.Resources.O15
+            SpinWheel()
+        ElseIf Number Mod 24 = 15 Then
+            pic1.Image = My.Resources.O16
+            SpinWheel()
+        ElseIf Number Mod 24 = 16 Then
+            pic1.Image = My.Resources.O17
+            SpinWheel()
+        ElseIf Number Mod 24 = 17 Then
+            pic1.Image = My.Resources.O18
+            SpinWheel()
+        ElseIf Number Mod 24 = 18 Then
+            pic1.Image = My.Resources.O19
+            SpinWheel()
+        ElseIf Number Mod 24 = 19 Then
+            pic1.Image = My.Resources.O20
+            SpinWheel()
+        ElseIf Number Mod 24 = 20 Then
+            pic1.Image = My.Resources.O21
+            SpinWheel()
+        ElseIf Number Mod 24 = 21 Then
+            pic1.Image = My.Resources.O22
+            SpinWheel()
+        ElseIf Number Mod 24 = 22 Then
+            pic1.Image = My.Resources.O23
+            SpinWheel()
+        ElseIf Number Mod 24 = 23 Then
+            pic1.Image = My.Resources.O24
+            SpinWheel()
+        ElseIf Number Mod 24 = 24 Then
+            pic1.Image = My.Resources.O25
+            SpinWheel()
+        End If
+
+    End Sub
+
+    Private Sub SpinWheel()
+        If Number = Int(Value / 1.5) Then
+            Timer.Interval = 150
+        ElseIf Number = Int(Value / 1.25) Then
+            Timer.Interval = 200
+        ElseIf Number = Int(Value / 1.1) Then
+            Timer.Interval = 350
+        ElseIf Number = Int(Value / 1.05) Then
+            Timer.Interval = 500
+        ElseIf Number = Value - 1 Then
+            Timer.Interval = 1000
+        End If
+
+
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Number = 0
+        Timer.Interval = 100
+        Randomize()
+        Value = Int((75 * Rnd()) + 50)
+        Timer.Enabled = True
+    End Sub
+End Class
